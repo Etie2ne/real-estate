d37b938f1260fd99ca2428d950526121adbf1824
Etie2ne <etiennemoutaux@hotmail.fr>
2017-02-03 15:02:00 +0100

    TP3

--- app.js
+++ app.js
@@ -26,9 +26,37 @@ app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
 
-var request = require( 'request' );
-request( 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s', function ( error, response, body ) {
+function callleboncoin() {
+
+    var url = 'https://www.leboncoin.fr/ventes_immobilieres/1087631966.htm?ca=12_s'
+
+    request( url, function ( error, response, html ) {
+        if ( !error && response.statusCode == 200 ) {
+
+            const $ = cheerio.load( html )
+
+            const lbcDataArray = $( 'section.properties span.value' )
+
+            let lbsData = {
+                price: parseInt( $( lbcDataArray.get( 0 ) ).text().replace( /\s/g, '' ), 10 ),
+                city: $( lbcDataArray.get( 1 ) ).text().trim().toLowerCase().replace( /\s/g, '-' ),
+                type: $( lbcDataArray.get( 2 ) ).text().trim().toLowerCase(),
+                surface: parseInt( $( lbcDataArray.get( 4 ) ).text().replace( /\s/g, '' ), 10 )
+            }
+            console.log( lbcData )
+        }
+        else {
+            console.log( error )
+        }
+    }
+
+
+
+
+
+/*var request = require( 'request' );
+request( 'https://www.leboncoin.fr/ventes_immobilieres/1087631966.htm?ca=12_s', function ( error, response, body ) {
     if ( !error && response.statusCode == 200 ) {
         console.log( body ) // Show the HTML for the Google homepage.
-    }
+    }*/
 })
