4c7bd9a55ace0851cc1e42c8fd8a15e5dbf441f7
Etie2ne <etiennemoutaux@hotmail.fr>
2017-02-03 13:38:33 +0100

    Start TP3

--- app.js
+++ app.js
@@ -25,3 +25,10 @@ app.get( '/', function ( req, res ) {
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+var request = require( 'request' );
+request( 'https://www.leboncoin.fr/ventes_immobilieres/1076257949.htm?ca=12_s', function ( error, response, body ) {
+    if ( !error && response.statusCode == 200 ) {
+        console.log( body ) // Show the HTML for the Google homepage.
+    }
+})
